8430dc86449e25fbb26715068affe4653570fdd0
ecp-alex <alex.mackenzie@englishcollege.cz>
2018-07-12 10:21:31 +0200

    amactive | r3 | show posts

--- wp-content/themes/amactive/index.php
+++ wp-content/themes/amactive/index.php
@@ -2,7 +2,17 @@
 <?php get_sidebar(); ?>
 
 <div class="contentMiddle" id="SpanRight">
-???
+    <?php
+        if( have_posts() ):
+            while ( have_posts() ): the_post(); ?>
+                <h3><?php the_title();?></h3>
+                <h3><?php the_content();?></h3>
+                <small><?php the_category();?></small>
+                <hr>
+            <?php
+            endwhile;
+        endif;
+    ?>
 </div>
 
 <?php get_footer(); ?>
