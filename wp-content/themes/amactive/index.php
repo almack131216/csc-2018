628a3a368ddee41f1a65f6d753710269b5088d07
ecp-alex <alex.mackenzie@englishcollege.cz>
2018-08-23 16:35:08 +0200

    JIC | pagination working | needs CR

--- wp-content/themes/amactive/index.php
+++ wp-content/themes/amactive/index.php
@@ -1,5 +1,5 @@
 <?php
-    $wp_query->set( 'posts_per_page', 9 );
+    $wp_query->set( 'posts_per_page', 13 );
     get_header();
 ?>
 
@@ -27,49 +27,41 @@
                 // if ( is_page() || is_single() ):
                 if ( $GLOBALS['pageType'] == 'page' || $GLOBALS['pageType'] == 'single' ):
                     // var_dump( get_post() );
-
                     the_post();
                     get_template_part('content', get_post_format());
-                    
-                    
-                    // $debugCount = 0;
-                    // while ( have_posts() ):
-                    // $debugCount++;
-                    // amactive_debug('$debugCount: '.$debugCount);
-                    //     the_post();                    
-                        
-                    // endwhile;
-                    // get_template_part('content', get_post_format());
-
                 else:
 
-                    // $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-                    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-                    echo '<br><strong>PAGED:</strong>'.$paged;
+                    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 0;
+                    $postsPerPage = 12;// see WP settings>reading
+                    $postOffset = $paged * $postsPerPage;
+                    amactive_debug('<strong>PAGED:</strong>'.$paged);
 
-                    $cpt_sale_status = 1;
+                    // $cpt_sale_status = 1;
                     $excludeCats = array(DV_category_IsSold_id, DV_category_News_id, DV_category_Testimonials_id, DV_category_Press_id);
-
+                    // amactive_debug('EXCLUDECATS 1: '.print_r($excludeCats));
                     // echo '??? > switch cat: '.$GLOBALS['postPageCategoryId'].' -> '.DV_category_IsSold_id;
 
                     if($GLOBALS['postPageCategoryId'] == DV_category_IsSold_id) {
                         //echo '?????? > switch cat: '.$GLOBALS['postPageCategoryId'].' -> '.DV_category_IsSold_id;
-                        $excludeCats = array(DV_category_News_id, DV_category_Testimonials_id, DV_category_Press_id);                      
-                        $GLOBALS['postPageCategoryId'] = DV_category_IsForSale_id;
-                        $cpt_sale_status = 2;
+                        // $excludeCats = array(DV_category_News_id, DV_category_Testimonials_id, DV_category_Press_id);                      
+                        // $GLOBALS['postPageCategoryId'] = DV_category_IsForSale_id;
+                        // $cpt_sale_status = 2;
                     }
 
                     $lookInCats = array($GLOBALS['postPageCategoryId']);
+
+                    /* IF subcategory is set... */
                     if($GLOBALS['postPageSubCategoryId']){                            
                         $lookInCats = array($GLOBALS['postPageCategoryId'], $GLOBALS['postPageSubCategoryId']);                            
                     }
 
+                    /* IF not on SOLD category... */
                     if ( $GLOBALS['postPageCategoryId'] != DV_category_IsSold_id ) {
-                        $lookInCats = array(2, 38);
+                        $lookInCats = array(2);
                         amactive_debug('FOR SALE: '.$GLOBALS['postPageCategoryId'].' -> '.$GLOBALS['postPageSubCategoryId']);
 
                         //REF: https://wordpress.stackexchange.com/questions/273523/include-posts-from-some-categories-while-excluding-from-others
-                        $argsXXX = array(
+                        $args = array(
                             'tax_query' => array(
                                 'relation' => 'AND', // logical relationship between taxonomy arrays
                                 array( // subcategories to exclude
@@ -87,12 +79,12 @@
                             )
                         );
 
-                        $args = array(                             
-                            'category'   => $lookInCats,
-                            // 'category__in'   => array(2, 38),
-                            'category__not_in'   => $excludeCats                           
+                        // $args = array(                             
+                        //     'category'   => $lookInCats,
+                        //     // 'category__in'   => array(2, 38),
+                        //     'category__not_in'   => $excludeCats                           
 
-                        );
+                        // );
 
                         if($GLOBALS['postPageSubCategoryId']){
                             $args2 = array( // subcategories to exclude
@@ -101,12 +93,13 @@
                                 'terms'         => $GLOBALS['postPageSubCategoryId'],
                             );
 
-                            array_push($argsXXX['tax_query'], $args2);
+                            array_push($args['tax_query'], $args2);
                         }
 
                     } else if ( $GLOBALS['postPageCategoryId'] == DV_category_IsSold_id ) {
                         
                         amactive_debug('SOLD: '.$GLOBALS['postPageCategoryId'].' > '.$GLOBALS['postPageSubCategoryId']);
+                        
 
                         $args = array(
                             'tax_query' => array(                                
@@ -114,37 +107,56 @@
                                     'taxonomy'      => 'category',
                                     'field'         => 'term_id',
                                     'terms'         => $lookInCats,
-                                    'operator' => 'AND'
+                                    'posts_per_page' => $postsPerPage,
+                                    'post_status' => array('publish'),
                                 )                                
                             )
                         );
+
+                        // $args = array(                             
+                        //     'cat'   => $lookInCats,
+                        //     'category__in'   => array(2, 38),
+                        //     'category__not_in'   => $excludeCats                           
+
+                        // );
                         
                     }
 
+                    // amactive_debug('LOOKINCATS: '.print_r($lookInCats));
+                    // amactive_debug('EXCLUDECATS: '.print_r($excludeCats));
+
                     $args2 = array(
-                        'paged' => $paged,
+                        // 'ignore_sticky_posts' => 1,
+                        'paged' => $paged,    
+                        // 'posts_per_page' => $postsPerPage,                    
+                        // 'offset'          => $postOffset,
                         'post_type' => 'post',
-                        'posts_per_page' => 12,
+                        'post_status' => array('publish'),
                         'orderby' => 'post_date',
                         'order' => 'DESC',
-                        'meta_query' => array(
-                            'relation' => 'AND',
-                            array(
-                                'key' => '_thumbnail_id',
-                                'compare' => 'EXISTS'
-                            )
-                        )
+                        // 'meta_query' => array(
+                        //     'relation' => 'AND',
+                        //     array(
+                        //         'key' => '_thumbnail_id',
+                        //         'compare' => 'EXISTS'
+                        //     )
+                        // )
                     );
-                    $args += $args2;
-
-                    
-                    
+                    $args += $args2;  
+                    // remove_all_filters( 'pre_get_posts' );                  
                     $query = new WP_Query( $args );
+                    // $query->set('posts_per_page', 12);
+                    // echo $GLOBALS['wp_query']->request;
 
                     if( $query->have_posts() ){
+                        // echo '???';
                         echo '<div class="row portfolio-wrap">';
+                        // $debugCount = 0;
+                        // echo '$debugCount: '.$debugCount;
                         while ( $query->have_posts() ):
-                            $query->the_post();                    
+                            // echo '<br>$debugCount: '.$debugCount;
+                            $query->the_post();    
+                            // echo get_the_title();                
                             echo '<div class="col-lg-4 col-md-4 col-sm-6 portfolio-item">';
                             get_template_part('content-grid-item', get_post_format());
                             echo '</div>';               
@@ -152,11 +164,12 @@
                         // REF: https://developer.wordpress.org/themes/functionality/pagination/
                         echo '<p>???'.wpbeginner_numeric_posts_nav().'</p>';
                         wp_pagenavi();
+
                         echo '</div>';
 
 
                     } else {
-                        // no posts found
+                        echo 'no posts found';
                     }
                     
                 endif;
